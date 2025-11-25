fb371f2fb446965a363aa143805c94780bec0c06
lavlesh jha <lavleshjha1001@gmail.com>
2025-11-25 21:53:36 +0530

    aks location changed

--- Environments/Dev/main.tf
+++ Environments/Dev/main.tf
@@ -9,27 +9,27 @@ locals {
 module "resource_group" {
   source   = "../../Modules/Azurerm_Resource_Group"
   rg_name  = "dev-todoapp-rglav"
-  location = "central india"
+  location = "East US"
   tags     = local.common_tags
 }
-module "storage_account" {
-  depends_on = [module.resource_group]
+# module "storage_account" {
+#   depends_on = [module.resource_group]
 
-  source                   = "../../Modules/Azurerm_Storage_Acnt"
-  stg_name                 = "devtodoappstglav"
-  rg_name                  = "dev-todoapp-rglav"
-  location                 = "central india"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  tags                     = local.common_tags
-}
+#   source                   = "../../Modules/Azurerm_Storage_Acnt"
+#   stg_name                 = "devtodoappstglav"
+#   rg_name                  = "dev-todoapp-rglav"
+#   location                 = "East US"
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+#   tags                     = local.common_tags
+# }
 module "container_registry" {
   depends_on = [module.resource_group]
 
   source   = "../../Modules/Azurerm_Container_Registry"
   acr_name = "lucktodoappacr2025"
   rg_name  = "dev-todoapp-rglav"
-  location = "central india"
+  location = "East US"
   tags     = local.common_tags
 }
 module "kubernetes_cluster" {
@@ -38,7 +38,7 @@ module "kubernetes_cluster" {
   source       = "../../Modules/Azurerm_K8s_Cluster"
   cluster_name = "dev-todoapp-aks-lav"
   rg_name      = "dev-todoapp-rglav"
-  location     = "central india"
+  location     = "East US"
   dns_prefix   = "dev-todoapp-aks-lav"
   node_count   = 2
   vm_size      = "Standard_D2s_v3"
@@ -51,7 +51,7 @@ module "sql_server" {
   source             = "../../Modules/Azurerm_SQL_Server"
   sql_server_name    = "devtodoappsqlsvrlav"
   rg_name            = "dev-todoapp-rglav"
-  location           = "central india"
+  location           = "East US"
   sql_admin_username = "sqladminlav"
   sql_admin_password = "Devopslav@1001"
   tags               = local.common_tags
